5dac9a79a5470f5c3bb4df4c6eb897db68c7569a
Lars Kronfält <lakr@mccc.se>
2026-04-11 18:12:37 +0200

    chore: force push

--- scripts/update_app_of_apps_repo.sh
+++ scripts/update_app_of_apps_repo.sh
@@ -9,4 +9,4 @@ GITEA_USER="$(kubectl -n gitea get secret gitea-admin -o jsonpath='{.data.admin-
 GITEA_PASS_ENCODED=$(python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.stdin.read().strip(), safe=''))" <<<"${GITEA_PASS}")
 git remote add gitappofapps http://${GIT_USER}:${GIT_PASS_ENCODED}@localhost:3000/${GIT_USER}/app-of-apps.git
 git remote set-url gitappofapps http://${GITEA_USER}:${GITEA_PASS_ENCODED}@localhost:3000/${GITEA_USER}/app-of-apps.git
-git push gitappofapps main:main
+git push gitappofapps main:main --force
