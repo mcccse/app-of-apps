dc0a609405e474b2c0005109b8905372d11fb230
Lars Kronfält <larkro@users.noreply.github.com>
2026-04-11 19:29:58 +0200

    add local path provisioner (#13)
    * chore: add kubecspace to dev
    
    * chore: push branch main for apps of apps
    
    * chore: force push
    
    * fix: add local-path-provisioner
    
    * chore: add local-path-provisioner to dev
    
    * chore: update Chart path
    
    * chore: ai tried again, now with templates
    
    * chore: label namespace to fix permission issues
    
    * chore: upgrade local-path-provisioner
    
    * chore: fix permissions for kubescape
    
    * chore: use current branch as source for pushing app-of-apps

--- scripts/update_app_of_apps_repo.sh
+++ scripts/update_app_of_apps_repo.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+BRANCH="$(git rev-parse --abbrev-ref HEAD)"
+
 GITEA_PASS="$(kubectl -n gitea get secret gitea-admin -o jsonpath='{.data.admin-password}' | base64 -d)"
 GITEA_USER="$(kubectl -n gitea get secret gitea-admin -o jsonpath='{.data.admin-username}' | base64 -d)"
 # GITEA_PASS_ENCODED=$(curl -Gso /dev/null -w "%{url_effective}" --data-urlencode "=${GITEA_PASS}" "" | cut -c3-)
@@ -9,4 +11,4 @@ GITEA_USER="$(kubectl -n gitea get secret gitea-admin -o jsonpath='{.data.admin-
 GITEA_PASS_ENCODED=$(python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.stdin.read().strip(), safe=''))" <<<"${GITEA_PASS}")
 git remote add gitappofapps http://${GIT_USER}:${GIT_PASS_ENCODED}@localhost:3000/${GIT_USER}/app-of-apps.git
 git remote set-url gitappofapps http://${GITEA_USER}:${GITEA_PASS_ENCODED}@localhost:3000/${GITEA_USER}/app-of-apps.git
-git push gitappofapps add-forgejo:main
+git push gitappofapps ${BRANCH}:main --force
