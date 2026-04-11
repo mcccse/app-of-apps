89407465c3a17878f8c49c212e8bbdedebd2515a
Lars Kronfält <lakr@mccc.se>
2026-04-11 16:58:13 +0200

    chore: use gitea

--- scripts/update_app_of_apps_repo.sh
+++ scripts/update_app_of_apps_repo.sh
@@ -1,16 +1,11 @@
 #!/usr/bin/env bash
 
-GIT_APP="forgejo"
-
-GIT_PASS="$(kubectl -n ${GIT_APP} get secret ${GIT_APP}-admin -o jsonpath='{.data.password}' | base64 -d)"
-GIT_USER="$(kubectl -n ${GIT_APP} get secret ${GIT_APP}-admin -o jsonpath='{.data.username}' | base64 -d)"
-GIT_PASS_ENCODED=$(curl -Gso /dev/null -w "%{url_effective}" --data-urlencode "=${GIT_PASS}" "" | cut -c3-)
-GIT_PASS_ENCODED=$(python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.stdin.read().strip(), safe=''))" <<< "${GIT_PASS}")
-
-# git remote set-url gitea http://${GIT_USER}:${GIT_PASS_ENCODED}@localhost:3000/admin/app-of-apps.git
-# git remote add gitea http://${GIT_USER}:${GIT_PASS_ENCODED}@localhost:3000/${GIT_USER}/app-of-apps.git
-# git remote set-url gitea "http://${GIT_USER}:${GIT_PASS}@localhost:3000/admin/app-of-apps.git"
-
-git remote add gitappofapps http://${GIT_USER}:${GIT_PASS_ENCODED}@localhost:3000/${GIT_USER}/app-of-apps.git
-git remote set-url gitappofapps http://${GIT_USER}:${GIT_PASS_ENCODED}@localhost:3000/${GIT_USER}/app-of-apps.git
-git push gitappofapps main:main
+GITEA_PASS="$(kubectl -n gitea get secret gitea-admin -o jsonpath='{.data.admin-password}' | base64 -d)"
+GITEA_USER="$(kubectl -n gitea get secret gitea-admin -o jsonpath='{.data.admin-username}' | base64 -d)"
+GITEA_PASS_ENCODED=$(curl -Gso /dev/null -w "%{url_effective}" --data-urlencode "=${GITEA_PASS}" "" | cut -c3-)
+# git remote set-url gitea http://${GITEA_USER}:${GITEA_PASS_ENCODED}@localhost:3000/admin/app-of-apps.git
+# git remote add gitea http://${GITEA_USER}:${GITEA_PASS_ENCODED}@localhost:3000/${GITEA_USER}/app-of-apps.git
+# git remote set-url gitea "http://${GITEA_USER}:${GITEA_PASS}@localhost:3000/admin/app-of-apps.git"
+GITEA_PASS_ENCODED=$(python3 -c "import urllib.parse, sys; print(urllib.parse.quote(sys.stdin.read().strip(), safe=''))" <<< "${GITEA_PASS}")
+git remote set-url gitea http://${GITEA_USER}:${GITEA_PASS_ENCODED}@localhost:3000/${GITEA_USER}/app-of-apps.git
+git push gitea add-metric-server:main
